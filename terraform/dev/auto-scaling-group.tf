4eb6df71783b5960df5c45711062fa92c339f405
opariffazman <ariff.azman@proton.me>
2025-02-20 11:36:59 +0800

    feat: initial implementation of basic application with auto scaling

---
+++ terraform/dev/auto-scaling-group.tf
@@ -0,0 +1,37 @@
+module "app_asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.1.0"
+
+  name = "${var.project_name}-asg"
+
+  min_size         = 0
+  max_size         = 1
+  desired_capacity = 1
+
+  wait_for_capacity_timeout = 0
+  health_check_type         = "ELB"
+  vpc_zone_identifier       = module.vpc.private_subnets
+
+  instance_type = "t2.micro"
+  image_id      = data.aws_ami.ubuntu.id
+  user_data     = base64encode(file("../../scripts/bash/app-userdata.sh"))
+
+  create_launch_template = true
+  launch_template_name   = "${var.project_name}-launch-template"
+
+  instance_refresh = {
+    strategy = "Rolling"
+    preferences = {
+      min_healthy_percentage = 50
+    }
+  }
+
+  traffic_source_attachments = {
+    app-alb = {
+      traffic_source_identifier = module.app_alb.target_groups["app-asg"].arn
+      traffic_source_type       = "elbv2"
+    }
+  }
+
+  tags = var.tags
+}
