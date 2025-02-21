d6f606679d1db5d327764d1f9a261b9fbd8bdf15
opariffazman <ariff.azman@proton.me>
2025-02-21 10:45:45 +0800

    feat: initial implementation of eks with calico helm chart

---
+++ terraform/dev/eks.tf
@@ -0,0 +1,66 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.33.1"
+
+  cluster_name    = "${var.project_name}-cluster"
+  cluster_version = "1.31"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  # Disable AWS VPC CNI for Calico
+  cluster_addons = {
+    vpc-cni = {
+      most_recent    = true
+      before_compute = true
+      configuration_values = jsonencode({
+        init = {
+          env = {
+            DISABLE_TCP_EARLY_DEMUX = true
+          }
+        }
+      })
+    }
+  }
+
+  eks_managed_node_groups = {
+    spot = {
+      name = "${var.project_name}-eks-spot"
+      
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t2.micro"]
+      capacity_type  = "SPOT"
+    }
+
+    ondemand = {
+      name = "${var.project_name}-eks-ondemand"
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t2.micro"]
+      capacity_type  = "ON_DEMAND"
+    }
+  }
+}
+
+resource "helm_release" "calico" {
+  name       = "calico"
+  repository = "https://docs.projectcalico.org/charts"
+  chart      = "calico"
+  namespace  = "kube-system"
+
+  values = [
+    jsonencode({
+      installation = {
+        kubernetesProvider = "EKS"
+      }
+    })
+  ]
+
+  depends_on = [module.eks]
+}
