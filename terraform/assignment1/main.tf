f2339ccc6b20a77aa7406c1233ad68a22f61c8cd
opariffazman <ariff.azman@proton.me>
2025-02-23 07:52:13 +0800

    refactor: added providers & reinitialized without eks

--- terraform/assignment1/main.tf
+++ terraform/assignment1/main.tf
@@ -1,3 +1,16 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.88.0"
+    }
+  }
+}
+
 terraform {
   backend "s3" {
     bucket         = "terraform-state-881490090314"
