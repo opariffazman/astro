a0341acea4bc9cba18388ec301ace10770c7602e
opariffazman <ariff.azman@proton.me>
2025-02-23 14:49:40 +0800

    feat: added web_iam_role

--- terraform/assignment1/iam.tf
+++ terraform/assignment1/iam.tf
@@ -8,7 +8,22 @@ module "app_iam_role" {
   custom_role_policy_arns = [
     "arn:aws:iam::aws:policy/AmazonRDSFullAccess",
     "arn:aws:iam::aws:policy/CloudWatchFullAccess",
-    "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+    "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess",
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  ]
+
+  trusted_role_services = ["ec2.amazonaws.com"]
+}
+
+module "web_iam_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "5.52.2"
+
+  create_role = true
+  role_name   = "${var.project_name}-web-role"
+
+  custom_role_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   ]
 
   trusted_role_services = ["ec2.amazonaws.com"]
