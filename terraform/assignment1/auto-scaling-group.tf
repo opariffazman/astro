1841cf9c2ddeb6dce9549e24953897e47c872f18
opariffazman <ariff.azman@proton.me>
2025-02-23 15:37:26 +0800

    feat: added proper tagging, timeouts & instance_refresh

--- terraform/assignment1/auto-scaling-group.tf
+++ terraform/assignment1/auto-scaling-group.tf
@@ -1,5 +1,6 @@
 resource "aws_autoscaling_group" "app" {
   name                = "${var.project_name}-app-asg"
+  force_delete        = true
   vpc_zone_identifier = module.vpc.private_subnets
   desired_capacity    = 1
   max_size            = 1
@@ -9,10 +10,29 @@ resource "aws_autoscaling_group" "app" {
     id      = aws_launch_template.app.id
     version = "$Latest"
   }
+
+  tag {
+    key                 = "Name"
+    value               = "${var.project_name}-app"
+    propagate_at_launch = true
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["tag"]
+  }
 }
 
 resource "aws_autoscaling_group" "web" {
   name                = "${var.project_name}-web-asg"
+  force_delete        = true
   vpc_zone_identifier = module.vpc.public_subnets
   desired_capacity    = 0
   max_size            = 1
@@ -21,5 +41,23 @@ resource "aws_autoscaling_group" "web" {
   launch_template {
     id      = aws_launch_template.web.id
     version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "${var.project_name}-web"
+    propagate_at_launch = true
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["tag"]
   }
 }
