8c6b7459d670e88662f7b73f3e71cd8f0c672dd0
opariffazman <ariff.azman@proton.me>
2025-02-23 21:08:53 +0800

    refactor: implemented modules usage

--- terraform/assignment1/auto-scaling-group.tf
+++ terraform/assignment1/auto-scaling-group.tf
@@ -1,67 +1,49 @@
-resource "aws_autoscaling_group" "app" {
-  name                = "${local.project_name}-app-asg"
-  force_delete        = true
+module "app_asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.1.0"
+
+  name = "${local.project_name}-app-asg"
+
   vpc_zone_identifier = module.vpc.private_subnets
-  desired_capacity    = 1
-  max_size            = 1
+  security_groups     = [module.app_security_group.security_group_id]
   min_size            = 0
+  max_size            = 1
+  desired_capacity    = 0
 
-  launch_template {
-    id      = aws_launch_template.app.id
-    version = aws_launch_template.app.latest_version
-  }
+  iam_instance_profile_arn = module.app_iam_role.iam_instance_profile_arn
 
-  tag {
-    key                 = "Name"
-    value               = "${local.project_name}-app"
-    propagate_at_launch = true
-  }
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
 
-  timeouts {
-    delete = "15m"
-  }
+  ignore_desired_capacity_changes = true
 
-  instance_refresh {
-    strategy = "Rolling"
-    preferences {
-      min_healthy_percentage = 50
-    }
-    triggers = ["tag"]
-  }
-
-  target_group_arns = [module.app_alb.target_groups["app-asg"].arn]
+  user_data = base64encode(templatefile("../../scripts/bash/userdata.sh", {
+    TIER_PORT = local.app_port
+    TIER_NAME = local.app_name
+  }))
 }
 
-resource "aws_autoscaling_group" "web" {
-  name                = "${local.project_name}-web-asg"
-  force_delete        = true
+module "web_asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.1.0"
+
+  name = "${local.project_name}-web-asg"
+
   vpc_zone_identifier = module.vpc.public_subnets
-  desired_capacity    = 0
-  max_size            = 1
+  security_groups     = [module.web_security_group.security_group_id]
   min_size            = 0
+  max_size            = 1
+  desired_capacity    = 1
 
-  launch_template {
-    id      = aws_launch_template.web.id
-    version = aws_launch_template.web.latest_version
-  }
+  iam_instance_profile_arn = module.web_iam_role.iam_instance_profile_arn
 
-  tag {
-    key                 = "Name"
-    value               = "${local.project_name}-web"
-    propagate_at_launch = true
-  }
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
 
-  timeouts {
-    delete = "15m"
-  }
+  ignore_desired_capacity_changes = true
 
-  instance_refresh {
-    strategy = "Rolling"
-    preferences {
-      min_healthy_percentage = 50
-    }
-    triggers = ["tag"]
-  }
-
-  target_group_arns = [module.web_alb.target_groups["web-asg"].arn]
+  user_data = base64encode(templatefile("../../scripts/bash/userdata.sh", {
+    TIER_PORT = local.web_port
+    TIER_NAME = local.web_name
+  }))
 }
