414c59119b60d224c3d57db8d6a98a32c85d61a1
opariffazman <ariff.azman@proton.me>
2025-02-23 19:22:15 +0800

    fix: corrected number params

--- terraform/assignment1/cloudwatch.tf
+++ terraform/assignment1/cloudwatch.tf
@@ -1,12 +1,12 @@
 resource "aws_cloudwatch_metric_alarm" "cpu_alarm" {
   alarm_name          = "${local.project_name}-high-cpu"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "2"
+  evaluation_periods  = 2
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
-  period              = "300"
+  period              = 300
   statistic           = "Average"
-  threshold           = "80"
+  threshold           = 80
   alarm_description   = "This metric monitors EC2 CPU utilization"
   alarm_actions       = [aws_sns_topic.sns.arn]
 
