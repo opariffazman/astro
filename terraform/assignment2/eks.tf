4110a20dd9e4b16742201b2b2a7793a59328e652
opariffazman <ariff.azman@proton.me>
2025-02-23 08:22:04 +0800

    refactor: removed helm usage & replaced vpc, subnet ids with data

--- terraform/assignment2/eks.tf
+++ terraform/assignment2/eks.tf
@@ -2,65 +2,19 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.33.1"
 
-  cluster_name    = var.project_name
-  cluster_version = "1.32"
+  cluster_name                   = var.project_name
+  cluster_version                = "1.32"
+  cluster_endpoint_public_access = true
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  enable_cluster_creator_admin_permissions = true
 
-  # Disable AWS VPC CNI for Calico
-  cluster_addons = {
-    vpc-cni = {
-      most_recent    = true
-      before_compute = true
-      configuration_values = jsonencode({
-        init = {
-          env = {
-            DISABLE_TCP_EARLY_DEMUX = "true"
-          }
-        }
-      })
-    }
+  cluster_compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
   }
 
-  eks_managed_node_groups = {
-    spot = {
-      name = "${var.project_name}-spot"
+  vpc_id     = data.terraform_remote_state.network.outputs.vpc_id
+  subnet_ids = data.terraform_remote_state.network.outputs.private_subnets
 
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-
-      instance_types = ["t2.micro"]
-      capacity_type  = "SPOT"
-    }
-
-    ondemand = {
-      name = "${var.project_name}-ondemand"
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-
-      instance_types = ["t2.micro"]
-      capacity_type  = "ON_DEMAND"
-    }
-  }
-}
-
-resource "helm_release" "calico" {
-  name       = "calico"
-  repository = "https://docs.projectcalico.org/charts"
-  chart      = "calico"
-  namespace  = "kube-system"
-
-  values = [
-    jsonencode({
-      installation = {
-        kubernetesProvider = "EKS"
-      }
-    })
-  ]
-
-  depends_on = [module.eks]
+  tags = var.tags
 }
