3c76236342a5165660e554989c8f78c55141a98c
opariffazman <ariff.azman@proton.me>
2025-02-23 21:10:02 +0800

    refactor: modified to use local.*port feat: added ssm sg

--- terraform/assignment1/security-groups.tf
+++ terraform/assignment1/security-groups.tf
@@ -8,17 +8,10 @@ module "web_security_group" {
 
   ingress_with_cidr_blocks = [
     {
-      from_port   = 80
-      to_port     = 80
+      from_port   = local.web_port
+      to_port     = local.web_port
       protocol    = "tcp"
       description = "HTTP"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      description = "HTTPS"
       cidr_blocks = "0.0.0.0/0"
     }
   ]
@@ -45,8 +38,8 @@ module "app_security_group" {
 
   ingress_with_source_security_group_id = [
     {
-      from_port                = 80
-      to_port                  = 80
+      from_port                = local.app_port
+      to_port                  = local.app_port
       protocol                 = "tcp"
       description              = "Application port"
       source_security_group_id = module.web_security_group.security_group_id
@@ -75,11 +68,40 @@ module "db_security_group" {
 
   ingress_with_source_security_group_id = [
     {
-      from_port                = 3306
-      to_port                  = 3306
+      from_port                = local.db_port
+      to_port                  = local.db_port
       protocol                 = "tcp"
       description              = "MySQL"
       source_security_group_id = module.app_security_group.security_group_id
+    }
+  ]
+
+  tags = local.tags
+}
+
+module "ssm_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+
+  name        = "${local.project_name}-ssm-sg"
+  description = "Security group for SSM"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = join(",", module.vpc.private_subnets_cidr_blocks)
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
     }
   ]
 
