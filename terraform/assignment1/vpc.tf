7470480c61c40c98a830901deecca8151ed13be0
opariffazman <ariff.azman@proton.me>
2025-02-23 22:50:35 +0800

    fix: added needed outputs

--- terraform/assignment1/vpc.tf
+++ terraform/assignment1/vpc.tf
@@ -31,3 +31,13 @@ resource "aws_vpc_endpoint" "ssm_endpoint" {
   ip_address_type     = "ipv4"
   subnet_ids          = module.vpc.private_subnets
 }
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
