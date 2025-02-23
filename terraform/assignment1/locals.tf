a78831b68b5f5467411f9ff4e6a48385e850ca7f
opariffazman <ariff.azman@proton.me>
2025-02-23 20:17:07 +0800

    fix: corrected private subnets

--- terraform/assignment1/locals.tf
+++ terraform/assignment1/locals.tf
@@ -10,7 +10,7 @@ locals {
   email_address = "ariff.tall814@passmail.net"
 
   vpc_cidr         = "10.0.0.0/16"
-  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets  = ["10.0.10.0/24", "10.0.20.0/24"]
   public_subnets   = ["10.0.101.0/24", "10.0.102.0/24"]
   database_subnets = ["10.0.201.0/24", "10.0.202.0/24"]
 
