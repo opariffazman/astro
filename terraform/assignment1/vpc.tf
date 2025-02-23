d5b82f80277c4776b1faf6368f83b219eee97ef7
opariffazman <ariff.azman@proton.me>
2025-02-23 21:10:17 +0800

    refactor: modified vpc endpoint for ssm

--- terraform/assignment1/vpc.tf
+++ terraform/assignment1/vpc.tf
@@ -21,33 +21,13 @@ module "vpc" {
   tags = local.tags
 }
 
-resource "aws_vpc_endpoint" "ssm" {
-  vpc_id             = module.vpc.vpc_id
-  service_name       = "com.amazonaws.${local.region}.ssm"
-  vpc_endpoint_type  = "Interface"
-  subnet_ids         = module.vpc.private_subnets
-  security_group_ids = [module.app_security_group.security_group_id]
-
-  private_dns_enabled = true
+resource "aws_vpc_endpoint" "ssm_endpoint" {
+  for_each            = local.ssm_services
+  vpc_id              = module.vpc.vpc_id
+  service_name        = each.value.name
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [module.ssm_security_group.security_group_id]
+  private_dns_enabled = false
+  ip_address_type     = "ipv4"
+  subnet_ids          = module.vpc.private_subnets
 }
-
-resource "aws_vpc_endpoint" "ssmmessages" {
-  vpc_id             = module.vpc.vpc_id
-  service_name       = "com.amazonaws.${local.region}.ssmmessages"
-  vpc_endpoint_type  = "Interface"
-  subnet_ids         = module.vpc.private_subnets
-  security_group_ids = [module.app_security_group.security_group_id]
-
-  private_dns_enabled = true
-}
-
-resource "aws_vpc_endpoint" "ec2messages" {
-  vpc_id             = module.vpc.vpc_id
-  service_name       = "com.amazonaws.${local.region}.ec2messages"
-  vpc_endpoint_type  = "Interface"
-  subnet_ids         = module.vpc.private_subnets
-  security_group_ids = [module.app_security_group.security_group_id]
-
-  private_dns_enabled = true
-}
-
