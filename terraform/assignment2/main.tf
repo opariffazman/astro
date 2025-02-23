b0ac02e26dcf549b237d126f8197749d41b4390e
opariffazman <ariff.azman@proton.me>
2025-02-23 08:22:19 +0800

    feat: initial commit

---
+++ terraform/assignment2/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.88.0"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "assignment2-state-881490090314"
+    key            = "assignment2.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "assignment2-state-lock"
+    encrypt        = true
+  }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket  = "assignment1-state-881490090314"
+    key     = "assignment1.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
