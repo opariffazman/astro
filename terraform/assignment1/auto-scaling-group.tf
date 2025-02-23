f5c2bdcb11541038268a601642fba52975ee02f6
opariffazman <ariff.azman@proton.me>
2025-02-23 22:44:05 +0800

    refactor: modified iam_instance_profile feat: added launch_template configs

--- terraform/assignment1/auto-scaling-group.tf
+++ terraform/assignment1/auto-scaling-group.tf
@@ -8,14 +8,18 @@ module "app_asg" {
   security_groups     = [module.app_security_group.security_group_id]
   min_size            = 0
   max_size            = 1
-  desired_capacity    = 0
+  desired_capacity    = 1
 
-  iam_instance_profile_arn = module.app_iam_role.iam_instance_profile_arn
+  iam_instance_profile_arn = aws_iam_instance_profile.app.arn
 
-  image_id      = data.aws_ami.ubuntu.id
+  image_id      = data.aws_ami.amazon_linux_2.id
   instance_type = "t2.micro"
 
   ignore_desired_capacity_changes = true
+
+  create_launch_template  = true
+  launch_template_name    = "${local.project_name}-app-launch-template"
+  launch_template_version = "$Latest"
 
   user_data = base64encode(templatefile("../../scripts/bash/userdata.sh", {
     TIER_PORT = local.app_port
@@ -33,17 +37,27 @@ module "web_asg" {
   security_groups     = [module.web_security_group.security_group_id]
   min_size            = 0
   max_size            = 1
-  desired_capacity    = 1
+  desired_capacity    = 0
 
-  iam_instance_profile_arn = module.web_iam_role.iam_instance_profile_arn
+  iam_instance_profile_arn = aws_iam_instance_profile.web.arn
 
-  image_id      = data.aws_ami.ubuntu.id
+  image_id      = data.aws_ami.amazon_linux_2.id
   instance_type = "t2.micro"
 
   ignore_desired_capacity_changes = true
+
+  create_launch_template  = true
+  launch_template_name    = "${local.project_name}-web-launch-template"
+  launch_template_version = "$Latest"
 
   user_data = base64encode(templatefile("../../scripts/bash/userdata.sh", {
     TIER_PORT = local.web_port
     TIER_NAME = local.web_name
   }))
+
+  network_interfaces = [
+    {
+      associate_public_ip_address = true
+    }
+  ]
 }
